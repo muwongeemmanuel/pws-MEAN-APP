64c9e64b0ae059efc19bf47d5a34e0dc6700ed21
Emmanuel Muwonge <muwongeemmanuel123@gmail.com>
2019-09-09 17:24:44 +0300

    trynna merge my branch

--- clientAngular/src/app/app-routing.module.ts
+++ clientAngular/src/app/app-routing.module.ts
@@ -10,7 +10,8 @@ const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
   { path: 'home', component: HomeComponent},
   { path: 'admin', component: AHomeComponent},
-  { path: 'aboutme', component: AboutMeComponent}
+  { path: 'aboutme', component: AboutMeComponent},
+  { path: 'aboutme', component: AboutMeComponen},
 ];
 
 @NgModule({
